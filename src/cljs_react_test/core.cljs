796a066c150d56252bea609ca0abf8c497dfb72d
Sebastian Bensusan <sbensu@gmail.com>
2015-04-03 16:12:54 +0200

    Initial commit

---
+++ src/cljs_react_test/core.cljs
@@ -0,0 +1,21 @@
+(ns ^:figwheel-always cljs-react-test.core
+    (:require[om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [data owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text data)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+
