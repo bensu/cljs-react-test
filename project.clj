6ac6aefbad0eb57b0b396fa71efaef50733b1bbb
Sebastian Bensusan <sbensu@gmail.com>
2015-05-29 17:27:08 +0200

    Excluded cljx from deps by moving kebab to :test profile

--- project.clj
+++ project.clj
@@ -4,13 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
-                 [org.clojure/clojurescript "0.0-3269"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-3308"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cljsjs/react-with-addons "0.12.2-4"]
                  [prismatic/dommy "1.0.0"]
-                 [camel-snake-kebab "0.3.1"]
-                 [org.omcljs/om "0.8.8" :exclusions [cljsjs/react]]]
+                 [camel-snake-kebab "0.3.1" :exclusions [com.keminglabs/cljx]]]
 
   :scm {:name "git"
         :url "https://github.com/bensu/cljs-react-test"}
@@ -19,9 +18,12 @@
 
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-doo "0.1.0-SNAPSHOT"]]
+  
+  :aliases {"test" ["with-profile" "test" "doo" "slimer" "test"]}
 
   :source-paths ["src"]
   
+  :profiles {:test {:dependencies [[org.omcljs/om "0.8.8" :exclusions [cljsjs/react]]]}}
   :cljsbuild {:builds 
               {:test {:source-paths ["src" "test"]
                              :compiler {:output-to "target/testable.js"
