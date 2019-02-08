632b9bd751575ff12e7731ccfe68b6cb401e478a
Alexander Ushanov <flawless@last-try.org>
2019-02-08 20:23:02 +0300

    Updated deps in project

--- project.clj
+++ project.clj
@@ -1,34 +1,37 @@
-(defproject org.clojars.mihaelkonjevic/cljs-react-test "0.1.5"
+(defproject org.clojars.mihaelkonjevic/cljs-react-test "0.1.6"
   :description "A ClojureScript wrapper around Reacts Test Utilities"
   :url "https://github.com/bensu/cljs-react-test"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojurescript "1.9.854"]
-                 [cljsjs/react-with-addons "15.6.1-0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.516"]
+                 [cljsjs/react "16.8.0-0"]
+                 [cljsjs/react-dom "16.8.0-0"]
                  [camel-snake-kebab "0.4.0"]]
 
   :scm {:name "git"
-        :url "https://github.com/bensu/cljs-react-test"}
+        :url "https://github.com/Flawless/cljs-react-test"}
 
   :deploy-repositories [["clojars" {:creds :gpg}]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-doo "0.1.6"]]
+            [lein-doo "0.1.10"]]
 
-  :aliases {"test" ["with-profile" "test" "doo" "chrome" "test"]}
+  :doo {:paths {:karma "./node_modules/karma/bin/karma"}}
+
+  :aliases {"test" ["with-profile" "test" "doo" "chrome-headless" "test"]}
 
   :source-paths ["src"]
 
   :profiles {:test {:dependencies [[prismatic/dommy "1.1.0"]
-                                   [cljsjs/react-dom "15.6.1-0" :exclusions [cljsjs/react]]]}}
+                                   [cljsjs/react-dom "16.8.0-0" :exclusions [cljsjs/react]]]}}
 
   :cljsbuild {:builds
               {:test {:source-paths ["src" "test"]
                       :compiler {:output-to "target/testable.js"
                                  :output-dir "target"
-                                 :main 'test.test-runner
+                                 :main test.test-runner
                                  :source-map true
                                  :optimizations :none
                                  :cache-analysis false
